c6c038770ca457d88fe6ec82ac370bfc8e4f8ec8
Danielcreux <jd2000_fs@hotmail.com>
2025-10-09 18:08:25 +0200

    actualizando

---
+++ Acceso a datos/001-Manejo de ficheros/006-Trabajo con ficheros/101-Ejercicios/001-Introduccion.py
@@ -0,0 +1,5 @@
+Vamos a hacer un simulacro de conector a csv
+
+clientes
+nombre,apellidos,email,direccion
+Jose Vicente,Carratala,info@jocarsa.com,La calle de Jose Vicente
---
+++ Acceso a datos/001-Manejo de ficheros/006-Trabajo con ficheros/101-Ejercicios/003-serializamos.py
@@ -0,0 +1,8 @@
+tupla = ("Joshue Daniel","Freire","info@daniel.com")
+
+archivo = open("clientes.csv",'a')
+cadena = ""
+for campo in tupla:
+    cadena += campo+","
+    archivo.write(cadena)
+    archivo.close()
